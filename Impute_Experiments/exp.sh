878508222c3d374838b10688d31d6535e30345f6
gketronDS <gketron@uci.edu>
2023-12-05 19:53:51 -0800

    Try fast generations in exp 2

--- Impute_Experiments/exp.sh
+++ Impute_Experiments/exp.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=Gabriel.Ketron@cshs.org
 #SBATCH --mail-user=gketron@uci.edu
 #SBATCH -o ./logs/output.%j_%a.out # STDOUT
-#SBATCH --array=3-4
+#SBATCH --array=5-6
 
 RUN=${SLURM_ARRAY_TASK_ID:-1}
 
