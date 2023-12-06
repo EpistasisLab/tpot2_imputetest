365a6c53ec7f096791abfbf8dca7c7fbfa479e1c
gketronDS <gketron@uci.edu>
2023-12-05 17:22:06 -0800

    Swap utils to check 3rd exp

--- Impute_Experiments/utils.py
+++ Impute_Experiments/utils.py
@@ -312,7 +312,7 @@ def loop_through_tasks(experiments, task_id_lists, base_save_folder, num_runs):
                 print("running experiment 1/3 - Does large hyperparameter space improve reconstruction accuracy over simple")
                 X_train_pandas = pd.DataFrame(X_train)
                 X_test_pandas = pd.DataFrame(X_test)
-                
+                '''
                 #Simple Impute 
                 SimpleImputeSpace = autoimpute.AutoImputer(added_missing=level, missing_type=type, model_names=['SimpleImputer'], n_jobs=48, show_progress=False, random_state=num_runs)
                 SimpleImputeSpace.fit(X_train_pandas)
@@ -432,7 +432,7 @@ def loop_through_tasks(experiments, task_id_lists, base_save_folder, num_runs):
 
                 with open(f"{save_folder}/tpot_space_scores.pkl", "wb") as f:
                     pickle.dump(tpot_space_scores, f)
-                '''
+                #'''
                 return
             except Exception as e:
                 trace =  traceback.format_exc() 
