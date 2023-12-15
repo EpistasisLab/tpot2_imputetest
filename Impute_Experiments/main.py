40d5fc70e62cca9d7e44e4f73b98af67cc967352
gketronDS <gketron@uci.edu>
2023-12-15 11:52:19 -0800

    Add simple back to compare

--- Impute_Experiments/main.py
+++ Impute_Experiments/main.py
@@ -32,7 +32,7 @@ def main():
     total_duration = 360000
 
     imputation_config_dict = {
-                #SimpleImputer: params_SimpleImpute, 
+                SimpleImputer: params_SimpleImpute, 
                 #IterativeImputer: params_IterativeImpute,
                 #KNNImputer: params_KNNImpute,
                 #RandomForestImputer: params_RandomForestImpute,
@@ -157,7 +157,7 @@ def main():
                     41671, 42183, 42192, 42225, 42477, 42493, 42545, 42636, 42688,
                     42712]
     '''
-    task_id_lists = [137]
+    task_id_lists = [32]
     print('starting loops')
     start = time.time()
     utils.loop_through_tasks(experiments, task_id_lists, base_save_folder, num_runs)
