8ccc7e8f9607aac4545961464266f9a3d27dcd12
gketronDS <gketron@uci.edu>
2023-12-15 11:36:53 -0800

    Remove estimators from iterative imputer

--- Impute_Experiments/param_grids.py
+++ Impute_Experiments/param_grids.py
@@ -13,12 +13,13 @@ def params_SimpleImpute(trial, name=None):
 
 def params_IterativeImpute(trial, name=None):
     params = {}
-    params['estimator'] = trial.suggest_categorical('estimator', ['Bayesian', 'RFR', 'Ridge', 'KNN'])
+    #params['estimator'] = trial.suggest_categorical('estimator', ['Bayesian', 'RFR', 'Ridge', 'KNN'])
     params['sample_posterior'] = trial.suggest_categorical('sample_posterior', [True, False])
     params['initial_strategy'] = trial.suggest_categorical('initial_strategy', ['mean', 'median', 'most_frequent', 'constant'])
     params['n_nearest_features'] = None
     params['imputation_order'] = trial.suggest_categorical('imputation_order', ['ascending', 'descending', 'roman', 'arabic', 'random'])
-    match params['estimator']: #do hyperparam search here too?
+    
+    '''match params['estimator']: #do hyperparam search here too?
         case 'Bayesian':
             params['estimator'] = sklearn.linear_model.BayesianRidge()
         case 'RFR':
@@ -26,9 +27,9 @@ def params_IterativeImpute(trial, name=None):
         case 'Ridge':
             params['estimator'] = sklearn.linear_model.Ridge()
         case 'KNN':
-            params['estimator'] = sklearn.neighbors.KNeighborsRegressor()
+            params['estimator'] = sklearn.neighbors.KNeighborsRegressor()'''
     param_grid = {
-        'estimator': params['estimator'],
+        #'estimator': params['estimator'],
         'sample_posterior': params['sample_posterior'],
         'initial_strategy': params['initial_strategy'],
         'n_nearest_features': params['n_nearest_features'],
